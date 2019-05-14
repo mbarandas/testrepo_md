1823e62dd94e7b3e403bfaa55c12f5a7d6f8e622
Marilia Barandas <marilia.barandas@fraunhofer.pt>
2019-05-14 09:44:40 +0100

    added setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,5 @@ setuptools.setup(
     version="0.0.1",
     author="Marilia",
     description="Library for testing",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/mbarandas/testrepo_md/",
 )
