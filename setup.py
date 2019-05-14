e1f7127796f9a971451087cec0e09f12c4097297
dmfolgado <duarte.m.folgado@gmail.com>
2019-05-14 09:55:33 +0100

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="testrepo_md",
-    version="0.0.1",
+    version="0.0.2",
     author="Marilia",
     description="Library for testing",
     url="https://github.com/mbarandas/testrepo_md/",
